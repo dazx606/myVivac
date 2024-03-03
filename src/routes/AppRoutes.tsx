0f95e1b9aa1762194accb09f296bc0ebe45fb36f
David Zuleta <d.a.z_606@hotmail.com>
2024-03-03 12:03:49 -0500

    import error fixed

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from '../pages/Home/Home'
 import { NavBar } from '../components/NavBar/NavBar'

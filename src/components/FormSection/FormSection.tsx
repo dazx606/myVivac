fef1937af3ba65b978f3f8278ea958597e459f7f
David Zuleta <d.a.z_606@hotmail.com>
2024-03-17 12:13:17 -0500

    form

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -10,8 +10,30 @@ const FormSection = () => {
                     <p>¿Quieres sumarte a la experiencia vivac? Te invitamos a un café y hablamos un poco.</p>
                 </div>
             </div>
-            <div className={styles.form}>
+            <div className={styles.formContainer}>
+                <div className={styles.form}>
+                    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                        <input type="text" placeholder='Nombre' className={styles.input}/>
+                        <input type="text" placeholder='Ciudad' className={styles.input}/>
+                    </div>
 
+                    <div style={{display:'flex', flexDirection:'row', justifyContent:'space-between'}}>
+                        <input type="text" placeholder='Email' className={styles.input}/>
+                        <input type="text" placeholder='Teléfono' className={styles.input}/>
+                    </div>
+
+                    <h2 className={styles.title}>Tipo de inversión</h2>
+
+                    <select  className={styles.input} style={{width:'100%'}}/>
+
+                    <div style={{display:'flex', flexDirection:'row', gap:'1ch'}}>
+                        <input type="checkbox" name="" id="" />
+                        <p>Acepto las políticas de privacidad y tratamiento de datos</p>
+                    </div>
+
+                    <button className={styles.button}>Enviar</button>
+
+                </div>
             </div>
         </div>
     )
